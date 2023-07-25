66a914c828558ca59264716b3e8745240fff7ebf
Hentioe <meow.i5.br@gmail.com>
2023-07-25 18:27:24 +0800

    Optimize subscribe/unsubscribe commands

--- lib/assistant_bot/chains/resp_unsubscribe_chain.ex
+++ lib/assistant_bot/chains/resp_unsubscribe_chain.ex
@@ -30,7 +30,7 @@ defmodule AssistantBot.RespUnSubscribeChain do
 
     full_name = String.trim(full_name)
 
-    [owner, repo] = String.split(full_name, "/")
+    [owner, repo, _rest] = String.split(full_name, "/")
 
     case unsubscribe(owner, repo) do
       {:ok, _} ->
@@ -56,13 +56,36 @@ defmodule AssistantBot.RespUnSubscribeChain do
   end
 
   @impl true
-  def handle(%{text: <<@command <> " pkgs " <> package_text::binary>>} = _message, context) do
+  def handle(%{text: <<@command <> " pkgs " <> args_text::binary>>} = _message, context) do
     %{chat_id: chat_id} = context
 
-    _ = package_text |> String.trim() |> String.split(" ") |> Enum.map(&remove_hex_pm_package/1)
+    _ = args_text |> parse_args() |> Enum.map(&remove_hex_pm_package/1)
 
     send_text(chat_id, commands_text("取消订阅成功。"), logging: true)
 
     {:ok, context}
+  end
+
+  @impl true
+  def handle(_message, context) do
+    %{chat_id: chat_id} = context
+
+    tunkown_args = commands_text("未知的参数，本命令格式如下：")
+
+    text = """
+    #{tunkown_args}
+
+    - <code>/unsubscribe repo [repo_full_name]</code>
+    - <code>/unsubscribe pkgs [package_name1] [package_name2] ...</code>
+    """
+
+    send_text(chat_id, text, parse_mode: "HTML", logging: true)
+
+    {:ok, context}
+  end
+
+  defp parse_args(args_text) do
+    # 按照空白字符分割并忽略空内容
+    args_text |> String.split(~r/\s+/) |> Enum.reject(&(&1 == ""))
   end
 end
