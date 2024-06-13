edae32740a6867fd0aceba75778950e34e21651a
Hentioe <meow.i5.br@gmail.com>
2024-06-13 12:48:53 +0800

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Assistant.MixProject do
     [
       app: :assistant,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
